1bde999562d6f613f6cdb778e2e2b2fa9f898372
Lancelot.exe <67932290+EdgarPlasencia@users.noreply.github.com>
2022-04-14 10:30:40 -0500

    Correccion Hilos

--- serverHilos.py
+++ serverHilos.py
@@ -1,56 +1,35 @@
-#Importamos las librerias
 import socket
+import os
 from _thread import *
-import threading
 
-print_lock = threading.Lock()
+ServerSideSocket = socket.socket()
+host = ''
+port = 2004
+ThreadCount = 0
 
-#Hilos
-def threaded(c):
-	while True:
+try:
+    ServerSideSocket.bind((host, port))
+except socket.error as e:
+    print(str(e))
+    
+print('Socket is listening..')
 
-		# Informacion que recive del cliente
-		data = c.recv(1024)
-		if not data:
-			print('Bye')
-			
-			# Liberar
-			print_lock.release()
-			break
+ServerSideSocket.listen(5)
 
-		data = data[::-1]
-		c.send(data)
-
-	#Cerrar conexion
-	c.close()
-
-
-def Main():
-    #localhost para el servidor
-	host = ""
-	port = 12345
-	s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-	s.bind((host, port))
-	print("socket binded to port", port)
-
-	#Escuchar
-	s.listen(5)
-	print("el socket esta escuchando")
-
-	#Bucle para el cliente
-	while True:
-
-		#Establecer con el cliente
-		c, addr = s.accept()
-
-		# lock acquired by client
-		print_lock.acquire()
-		print('Connected to :', addr[0], ':', addr[1])
-
-		# Iniciar nuevo hilo y devolver el identificador
-		start_new_thread(threaded, (c,))
-	s.close()
-
-
-if __name__ == '__main__':
-	Main()
+def multi_threaded_client(connection):
+    connection.send(str.encode('Server is working:'))
+    while True:
+        data = connection.recv(2048)
+        response = 'Server message: ' + data.decode('utf-8')
+        if not data:
+            break
+        connection.sendall(str.encode(response))
+    connection.close()
+    
+while True:
+    Client, address = ServerSideSocket.accept()
+    print('Connected to: ' + address[0] + ':' + str(address[1]))
+    start_new_thread(multi_threaded_client, (Client, ))
+    ThreadCount += 1
+    print('Thread Number: ' + str(ThreadCount))
+ServerSideSocket.close()
