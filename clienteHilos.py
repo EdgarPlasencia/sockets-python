9e9217ecc93014738581b7f004695524d746bb3c
Yinsito <103659476+Yinsito@users.noreply.github.com>
2022-04-14 10:33:43 -0500

    Add files via upload

--- clienteHilos.py
+++ clienteHilos.py
@@ -1,41 +1,16 @@
-# importamos el modulo socket
 import socket
-
-
-def Main():
-	# local host IP ''
-	host = '172.17.37.100'
-
-	# definimos los puertos
-	port = 12345
-
-	s = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
-
-	# conecccion del servidor :v
-	s.connect((host,port))
-
-	# servidor mensaje 
-	message = "Hola desde el cliente"
-	while True:
-
-		# 
-		s.send(message.encode('ascii'))
-
-		# 
-		data = s.recv(1024)
-
-		# escribe el mensaje recivido
-		# aquí sería un reverso del mensaje enviado
-		print('Received from the server :',str(data.decode('ascii')))
-
-		# Preguntar a la clienta si quiere continuar
-		ans = input('\nQuieres continuar(y/n) :')
-		if ans == 'y':
-			continue
-		else:
-			break
-	# close the connection
-	s.close()
-
-if __name__ == '__main__':
-	Main()
+ClientMultiSocket = socket.socket()
+host = '172.17.37.100'
+port = 2004
+print('Esperando coneccion de respuesta')
+try:
+    ClientMultiSocket.connect((host, port))
+except socket.error as e:
+    print(str(e))
+res = ClientMultiSocket.recv(1024)
+while True:
+    Input = input('Hey there: ')
+    ClientMultiSocket.send(str.encode(Input))
+    res = ClientMultiSocket.recv(1024)
+    print(res.decode('utf-8'))
+ClientMultiSocket.close()
