93d94a63ce13c97b172d7a30af93946329cee438
Lancelot.exe <67932290+EdgarPlasencia@users.noreply.github.com>
2022-04-14 21:48:39 -0500

    Referencias del codigo

--- clienteHilos.py
+++ clienteHilos.py
@@ -1,3 +1,6 @@
+#Referencias del codigo
+#April 10, 2022 by Digamber
+#https://www.positronx.io/create-socket-server-with-multiple-clients-in-python/
 import socket
 ClientMultiSocket = socket.socket()
 host = '172.17.37.100'
